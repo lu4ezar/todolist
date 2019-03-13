11b4f7331f2eede5a81d522b9c1b7cca9caff2c3
lu4ezar <misha-shisha@mail.ru>
2019-03-14 02:57:58 +0700

    form

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import PropTypes from 'prop-types';
 import Item from '../Item';
@@ -30,41 +29,6 @@ class ReactForm extends React.Component {
 
 	handleChange = e => {
 		let { name, value, type, checked } = e.target;
-=======
-// @ts-check
-import React from 'react';
-import PropTypes from 'prop-types';
-import Item from '../Item';
-import { Form, GridColumn, Label, Input } from 'semantic-ui-react';
-
-const selectOptions = [
-	{
-		key: '1',
-		value: 'normal',
-		text: 'Normal'
-	},
-	{
-		key: '2',
-		value: 'important',
-		text: 'Important'
-	},
-	{
-		key: '3',
-		value: 'veryImportant',
-		text: 'Very important'
-	}
-];
-
-class ReactForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { ...new Item() };
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	handleChange(data) {
-		let { name, value, type, checked } = data;
->>>>>>> e6afdeeb49771ecc0ff7eee46e25ee3538fc0365
 		value = type === 'checkbox' ? checked : value;
 		this.setState({
 			[name]: value
@@ -93,7 +57,6 @@ class ReactForm extends React.Component {
 
 	render() {
 		return (
-<<<<<<< HEAD
 			<ModalWindow
 				onHide={this.props.close}
 				title={this.props.mode === 'edit' ? 'Edit' : 'Add'}
@@ -180,68 +143,12 @@ class ReactForm extends React.Component {
 					</>
 				}
 			/>
-=======
-			<Form
-				onSubmit={() => this.props.handleSubmit(this.state, this.props.index)}
-			>
-				<Form.Input
-					label="Task"
-					name="task"
-					type="text"
-					placeholder="task goes here"
-					value={this.state.task}
-					onChange={(e, data) => this.handleChange(data)}
-					autoFocus
-				/>
-				<Form.TextArea
-					label="Description"
-					name="description"
-					type="textarea"
-					placeholder="add description"
-					value={this.state.description}
-					onChange={(e, data) => this.handleChange(data)}
-				/>
-				<GridColumn>
-					<Form.Dropdown
-						name="priority"
-						value={this.state.priority}
-						placeholder="Priority"
-						options={selectOptions}
-						onChange={(e, data) => this.handleChange(data)}
-						selection
-					/>
-				</GridColumn>
-				<Label sm={2} content="Complete until: " />
-				<Input
-					type="date"
-					name="date"
-					value={this.state.date}
-					onChange={(e, data) => this.handleChange(data)}
-				/>
-				<Input
-					type="time"
-					name="time"
-					value={this.state.time}
-					onChange={(e, data) => this.handleChange(data)}
-				/>
-				<Form.Button
-					type="submit"
-					disabled={!this.state.task || !this.state.description}
-				>
-					{this.props.mode === 'edit' ? 'Save changes' : 'Ok'}
-				</Form.Button>
-			</Form>
->>>>>>> e6afdeeb49771ecc0ff7eee46e25ee3538fc0365
 		);
 	}
 }
 
 ReactForm.propTypes = {
-<<<<<<< HEAD
 	handleSubmit: PropTypes.func
-=======
-	handleSubmit: PropTypes.func.isRequired
->>>>>>> e6afdeeb49771ecc0ff7eee46e25ee3538fc0365
 };
 
 export default ReactForm;
