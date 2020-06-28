96fa02699533d1d5c0c4d980455c5bcd1b87f6cf
lu4ezar <misha-shisha@mail.ru>
2020-06-28 17:16:51 +0700

    change Drawer's close icon

--- src/elements/Drawer.jsx
+++ src/elements/Drawer.jsx
@@ -1,5 +1,12 @@
 // @flow
 import * as React from "react";
+import {
+  IconButton,
+  Divider,
+  Container,
+  Drawer as CoreDrawer,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 import styled from "styled-components";
 import { ListColor } from "../utils/color";
 
@@ -17,8 +24,16 @@ const Content = styled.div`
   background-color: ${ListColor.background};
 `;
 
+const Drawer = ({ side, open, toggleDrawer, children }: Props): React.Node => (
+  <CoreDrawer anchor={side} open={open} onClose={toggleDrawer}>
     <Content side={side}>
+      <IconButton onClick={toggleDrawer}>
+        <CloseIcon />
+      </IconButton>
     </Content>
     <Divider />
+    <Container maxWidth={false}>{children}</Container>
+  </CoreDrawer>
+);
 
 export default Drawer;
