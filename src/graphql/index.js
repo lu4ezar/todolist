8b7344020e958fea42960030441fedaf9fda864f
lu4ezar <misha-shisha@mail.ru>
2020-08-02 22:39:59 +0700

    fix server address

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://todo-server-self.vercel.app/graphql",
+  uri: "https://todo-gql-server.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
