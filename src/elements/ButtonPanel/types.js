d1bccd20c5f159445e39f40a6366c807d2e2b653
lu4ezar <misha-shisha@mail.ru>
2020-10-17 23:42:39 +0700

    small code style fix

--- src/elements/ButtonPanel/types.js
+++ src/elements/ButtonPanel/types.js
@@ -2,5 +2,7 @@ import { Id, Todo } from "../../types/todo";
 
 export type ButtonPanelProps = {
   todo: Todo,
-  toggle: (id: Id) => void,                       deleteTodo: (id: Id) => void,                   showTodo: (id: Id, string) => void,
+  toggle: (id: Id) => void,
+  deleteTodo: (id: Id) => void,
+  showTodo: (id: Id, string) => void,
 };
