2bc112b4de2aa787988bcb5c77e70d528d6bbb20
lu4ezar <misha-shisha@mail.ru>
2020-10-21 21:14:37 +0700

    create folder for App

---
+++ src/components/App/styles.js
@@ -0,0 +1,12 @@
+import { Container } from "@material-ui/core";
+import styled from "styled-components";
+
+export const StyledContainer = styled(Container)`
+  display: flex;
+  flexdirection: column;
+  height: 100%;
+`;
+export const StyledApp = styled.div`
+  width: 100%;
+  height: 100%;
+`;
