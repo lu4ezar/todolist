227f265212e288198598dc323f639cce04ddc3d2
lu4ezar <misha-shisha@mail.ru>
2020-11-01 00:55:16 +0700

    enable Apollo dev tools

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://todo-gql-server.herokuapp.com/graphql",
   cache: new InMemoryCache(),
+  connectToDevTools: true,
 });
 
 export default client;
