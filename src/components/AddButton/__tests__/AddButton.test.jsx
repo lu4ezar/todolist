2bd6ac75c050e343887c43787a5f7010bdc9c76d
lu4ezar <misha7shisha@gmail.com>
2020-11-02 15:30:55 +0700

    AddButton test

---
+++ src/components/AddButton/__tests__/AddButton.test.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import AddButton from "../AddButton";
+
+const props = {
+  setMode: jest.fn(),
+};
+
+describe("Add Button", () => {
+  it("renders as expected", () => {
+    expect(render(<AddButton {...props} />)).toMatchSnapshot();
+  });
+  it("calls setMode prop function", () => {
+    const container = render(<AddButton {...props} />);
+    const addButton = container.getByTitle(/add/i);
+    fireEvent.click(addButton);
+    expect(props.setMode).toBeCalled();
+  });
+});
