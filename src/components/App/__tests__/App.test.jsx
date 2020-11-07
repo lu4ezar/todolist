21b1fc4253e87373369567c69376306bf896617d
lu4ezar <misha-shisha@mail.ru>
2020-11-07 22:58:00 +0700

    update App test

--- src/components/App/__tests__/App.test.jsx
+++ src/components/App/__tests__/App.test.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from "../../../utils/test-utils.js";
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "../../../utils/test-utils";
+
 import App from "../App";
 
 describe("App", () => {
   it("should render without error", () => {
     render(<App />);
+  });
+  it("should show and hide side panel on button click", async () => {
+    render(<App />);
+    expect(screen.queryByText(/filter/i)).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: /menu/i }));
+    expect(screen.getByText(/filter/i)).toBeInTheDocument();
+    userEvent.click(screen.getByRole("button", { name: /close/i }));
+    await waitForElementToBeRemoved(screen.getByText(/filter/i));
+    expect(screen.queryByText(/filter/i)).not.toBeInTheDocument();
   });
 });
