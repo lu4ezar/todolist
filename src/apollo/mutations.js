2f5483850ec1168e04ed7272956a4147ba26c738
lu4ezar <misha-shisha@mail.ru>
2020-11-09 21:46:41 +0700

    add Todo fields, proper types for mutations.js

--- src/apollo/mutations.js
+++ src/apollo/mutations.js
@@ -1,22 +1,27 @@
 // @flow
 import { gql } from "@apollo/client";
+import type { DocumentNode } from "graphql";
 
-export const CREATE_TODO = gql`
+export const CREATE_TODO: DocumentNode = gql`
   mutation createTodo($input: CreateTodoInput!) {
     createTodo(input: $input) {
       id
       title
+      description
+      status
+      priority
+      created
     }
   }
 `;
-export const UPDATE_TODO = gql`
+export const UPDATE_TODO: DocumentNode = gql`
   mutation updateTodo($id: ID!, $input: UpdateTodoInput!) {
     updateTodo(id: $id, input: $input) {
       title
     }
   }
 `;
-export const DELETE_TODO = gql`
+export const DELETE_TODO: DocumentNode = gql`
   mutation deleteTodo($id: ID!) {
     deleteTodo(id: $id) {
       title
