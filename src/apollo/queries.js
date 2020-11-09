eb5ae0c98a0c3ea1c84b6440fea2dd17d676b30b
lu4ezar <misha-shisha@mail.ru>
2020-11-09 21:49:27 +0700

    add types for queries

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -1,6 +1,7 @@
 import { gql } from "@apollo/client";
+import type { DocumentNode } from "graphql";
 
-export const GET_TODO = gql`
+export const GET_TODO: DocumentNode = gql`
   query GetTodo($id: ID!) {
     todo(id: $id) {
       id
@@ -13,7 +14,7 @@ export const GET_TODO = gql`
   }
 `;
 
-export const GET_TODOS = gql`
+export const GET_TODOS: DocumentNode = gql`
   query GetTodos {
     todos {
       id
