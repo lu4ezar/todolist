bfdc008fe67dc22f766af5b7d70c9d016b3b134b
lu4ezar <misha-shisha@mail.ru>
2020-11-18 21:03:00 +0700

    fix Filter stories

--- src/components/Filter/Filter.stories.jsx
+++ src/components/Filter/Filter.stories.jsx
@@ -1,23 +1,34 @@
-
-import React from 'react';
-import Filter from './Filter';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import Filter from "./Filter";
 
 export default {
   component: Filter,
-  title: 'Filter',
+  title: "Filter",
+  argTypes: { setFilter: { action: "clicked" } },
 };
 
-const Template = args => <Filter {...args} />;
+const Template = (args) => <Filter {...args} setFilter={action("changed")} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  setFilter,                                           completedCount,
-    expiredCount,                                        filter: {
-      master: { status: masterFilterStatus },              priority: { status: priorityFilterStatus, value: priorityFilterValue },                                   completed: { status: completedFilterStatus, val
-ue: completedFilterValue },                                expired: { status: expiredFilterStatus, value: expiredFilterValue }
-    }
+  filter: {
+    master: { status: false },
+    priority: { status: false, value: ["NORMAL"] },
+    completed: { status: false, value: false },
+    expired: { status: false, value: false },
+  },
 };
 
 export const Enabled = Template.bind({});
-Enabled.args = {};
-
+Enabled.args = {
+  ...Default.args.filter,
+  filter: {
+    master: { status: true },
+    priority: { status: false, value: ["NORMAL"] },
+    completed: { status: false, value: false },
+    expired: { status: false, value: false },
+  },
+  completedCount: 4,
+  expiredCount: 2,
+};
