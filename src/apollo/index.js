fe23579167a428b7ef61a4591b3e1b50e66b8eb8
lu4ezar <misha-shisha@mail.ru>
2020-11-22 15:32:02 +0700

    add dev server address

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,8 +1,13 @@
 import { ApolloClient } from "@apollo/client";
 import cache from "./cache";
 
+const uri =
+  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:4000/graphql"
+    : "https://todo-gql-server.herokuapp.com/graphql";
+
 const client = new ApolloClient({
-  uri: "https://todo-gql-server.herokuapp.com/graphql",
+  uri,
   cache,
   connectToDevTools: true,
 });
