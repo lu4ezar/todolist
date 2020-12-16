4e953ea76303663b079244349713bd8c9c2d3cec
lu4ezar <misha-shisha@mail.ru>
2020-12-16 19:31:26 +0700

    cleanup redux stuff

--- src/containers/UndoRedoContainer.js
+++ src/containers/UndoRedoContainer.js
@@ -1,18 +1,1 @@
 // @flow
-import { connect } from "react-redux";
-import { ActionCreators } from "redux-undo";
-import UndoRedo from "../components/UndoRedo";
-import type { Dispatch, State } from "../types";
-
-const mapStateToProps = (state: State) => ({
-  canUndo: state.todos.past.length,
-  canRedo: state.todos.future.length,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  undo: () => dispatch(ActionCreators.undo()),
-  redo: () => dispatch(ActionCreators.redo()),
-});
-
-// $FlowFixMe
-export default connect(mapStateToProps, mapDispatchToProps)(UndoRedo);
