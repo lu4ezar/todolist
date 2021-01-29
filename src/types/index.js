32167424da1e1d6b97ca6d27818b8fc2ca8be91c
lu4ezar <misha-shisha@mail.ru>
2021-01-29 23:12:02 +0700

    cleanup types/index

--- src/types/index.js
+++ src/types/index.js
@@ -1,19 +1,1 @@
 // @flow
-import type { TodosStateWithHistory, TodosAction } from "./todos";
-import type { FilterState, FilterAction } from "./filter";
-import type { CurrentTodoIdState, CurrentTodoIdAction } from "./currentTodoId";
-import type { ModeState, ModeAction } from "./mode";
-import type { NotificationState, NotificationAction } from "./notification";
-
-export type State = TodosStateWithHistory &
-  FilterState &
-  CurrentTodoIdState &
-  ModeState &
-  NotificationState;
-
-export type Action =
-  | TodosAction
-  | FilterAction
-  | CurrentTodoIdAction
-  | ModeAction
-  | NotificationAction;
