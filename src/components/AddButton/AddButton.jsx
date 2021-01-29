e62a78fc4119562697a282216f3bf49efe384fa8
lu4ezar <misha-shisha@mail.ru>
2021-01-29 22:54:41 +0700

    add 'Add Checklist' variant to AddButton component

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -1,13 +1,16 @@
 // @flow
 import * as React from "react";
-import AddIcon from "@material-ui/icons/Add";
-import Fab from "./styles";
+import { ListPlus, PlusCircle } from "phosphor-react";
+import { Fab } from "@material-ui/core";
 import type { Props } from "./types";
 
 const AddButton = ({ setMode, entity }: Props): React.Node => (
-  <Fab color="secondary" title="add todo" onClick={setMode}>
-    <AddIcon />
-    {entity}
+  <Fab color="primary" title={`add ${entity}`} onClick={setMode}>
+    {entity === "todo" ? (
+      <PlusCircle color="#8be4ff" weight="thin" size={48} />
+    ) : (
+      <ListPlus color="#8be4ff" weight="thin" size={48} />
+    )}
   </Fab>
 );
 
