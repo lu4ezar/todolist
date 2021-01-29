8531333cc8a84b3bfc51faaec4883becfbffb905
lu4ezar <misha-shisha@mail.ru>
2021-01-29 23:00:32 +0700

    add Checklist color

--- src/utils/color.js
+++ src/utils/color.js
@@ -12,6 +12,16 @@ export const TodoColor = {
   expiredBackground: "#aa0000",
 };
 
+export const ChecklistColor = {
+  color: "#383720",
+  background: "#d0d4db",
+  dragBackground: "#568171",
+  completedColor: "#003d00",
+  completedBackground: "#55aa7f",
+  expiredColor: "#440000",
+  expiredBackground: "#aa0000",
+};
+
 export const AppColor = {
   color: "#000",
   background: "#edecea",
