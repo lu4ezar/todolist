7789c57201f291ad142349d37422185560869c87
lu4ezar <misha-shisha@mail.ru>
2021-01-31 18:55:13 +0700

    update List container

--- src/containers/List.jsx
+++ src/containers/List.jsx
@@ -7,18 +7,17 @@ import filterEntity from "../utils/filterTodo";
 import { filterVar } from "../apollo/cache";
 
 export default (): React.Node => {
-  const { data: { todos = [] } = {}, loading, error } = useQuery(GET_ALL);
+  const { data = {}, loading, error } = useQuery(GET_ALL);
+  const { todos = [], checklists = [] } = data;
 
   const filter = filterVar();
 
-  const visibleList = todos.length
-    ? todos.reduce(
-        (acc, entity) =>
-          filter.master.completed
-            ? acc.concat(filterEntity(entity, filter))
-            : acc.concat(entity),
-        []
-      )
-    : [];
+  const visibleList = filter.master.status
+    ? [
+        ...todos.filter((todo) => filterEntity(todo, filter)),
+        ...checklists.filter((checklist) => filterEntity(checklist, filter)),
+      ]
+    : [...todos, ...checklists];
+
   return <WrappedList list={visibleList} loading={loading} error={error} />;
 };
