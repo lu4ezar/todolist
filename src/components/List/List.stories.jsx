d3ae38fcbada516b55251b09b5353762d3b85e90
lu4ezar <misha-shisha@mail.ru>
2021-02-22 10:13:37 +0700

    update List story

--- src/components/List/List.stories.jsx
+++ src/components/List/List.stories.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import List from "./List";
+import { DragDropContext } from "react-beautiful-dnd";
+import List from "./DnDWrapper";
 import * as TodoStories from "../Todo/Todo.stories";
+import { GET_ALL } from "../../apollo/queries";
+import {
+  lowPriorTodo,
+  normalPriorTodo,
+  normalPriorChecklist,
+} from "../../utils/test-utils";
 
 export default {
   component: List,
   title: "List",
-  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
 };
 
-const Template = (args) => <List {...args} />;
+const Template = (args) => (
+  <DragDropContext>
+    <List {...args} />
+  </DragDropContext>
+);
 
 export const Default = Template.bind({});
 Default.parameters = {
@@ -29,15 +39,46 @@ Default.parameters = {
   },
 };
 
-/* export const WithPinnedTasks = Template.bind({});
-WithPinnedTasks.args = {
-  // Shaping the stories through args composition.
-  // Inherited data coming from the Default story.
-  tasks: [
-    ...Default.args.tasks.slice(0, 5),
-    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+Default.args = {
+  list: [
+    {
+      ...TodoStories.Default.args.todo,
+      id: "1",
+      title: "Task 1",
+      __typename: "Todo",
+    },
+    {
+      ...TodoStories.Default.args.todo,
+      id: "2",
+      title: "Task 2",
+      __typename: "Todo",
+    },
+    {
+      ...TodoStories.Default.args.todo,
+      id: "3",
+      title: "Task 3",
+      __typename: "Todo",
+    },
+    {
+      ...TodoStories.Default.args.todo,
+      id: "4",
+      title: "Task 4",
+      __typename: "Todo",
+    },
+    {
+      ...TodoStories.Default.args.todo,
+      id: "5",
+      title: "Task 5",
+      __typename: "Todo",
+    },
+    {
+      ...TodoStories.Default.args.todo,
+      id: "6",
+      title: "Task 6",
+      __typename: "Todo",
+    },
   ],
-}; */
+};
 
 export const Loading = Template.bind({});
 Loading.args = {
@@ -47,8 +88,6 @@ Loading.args = {
 
 export const Empty = Template.bind({});
 Empty.args = {
-  // Shaping the stories through args composition.
-  // Inherited data coming from the Loading story.
   ...Loading.args,
   loading: false,
 };
