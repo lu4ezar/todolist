7398f8c88185fb04d262648a604b921e8f5244be
lu4ezar <misha-shisha@mail.ru>
2021-02-22 10:07:15 +0700

    add withKnobs decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { MockedProvider } from "@apollo/client/testing";
+import { withKnobs } from "@storybook/addon-knobs";
 
+export const decorators = [withKnobs()];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   apolloClient: {
