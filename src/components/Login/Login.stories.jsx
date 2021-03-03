d36537e07eddd9b000f8b24e4f901013accbcc9c
lu4ezar <misha-shisha@mail.ru>
2021-03-04 06:09:10 +0700

    create dummy story for Login

---
+++ src/components/Login/Login.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./Login";
+
+export default {
+  component: Login,
+  title: "Login",
+};
+
+const Template = () => <Login />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Enabled = Template.bind({});
+Enabled.args = {};
