85bb2756dd3d48231437743793789565b54dcaa2
lu4ezar <misha-shisha@mail.ru>
2021-03-08 06:20:35 +0700

    remove AddButton container

--- src/components/AddButton/AddButton.jsx
+++ src/components/AddButton/AddButton.jsx
@@ -2,14 +2,22 @@
 import * as React from "react";
 import { ListPlus, PlusCircle } from "phosphor-react";
 import { Fab } from "@material-ui/core";
+import { entityVar, modeVar } from "../../apollo/cache";
 import type { Props } from "./types";
 
-const AddButton = ({ setMode, entity }: Props): React.Node => (
-  <Fab color="primary" title={`add ${entity}`} onClick={setMode}>
+const AddButton = ({ entity = "Checklist", size = 48 }: Props): React.Node => (
+  <Fab
+    color="primary"
+    title={`add ${entity}`}
+    onClick={() => {
+      entityVar(entity);
+      modeVar("form");
+    }}
+  >
     {entity === "todo" ? (
-      <PlusCircle color="#8be4ff" weight="thin" size={48} />
+      <PlusCircle color="#8be4ff" weight="thin" size={size} />
     ) : (
-      <ListPlus color="#8be4ff" weight="thin" size={48} />
+      <ListPlus color="#8be4ff" weight="thin" size={size} />
     )}
   </Fab>
 );
